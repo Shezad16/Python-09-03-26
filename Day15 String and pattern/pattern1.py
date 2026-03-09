31fb1ccd4af751d13b8d5488422068e3869c1c26
Shezad Rayeen <shejadrayeen02@gmail.com>
2026-03-09 10:16:36 +0000

    left pyramid

--- Day15 String and pattern/pattern1.py
+++ Day15 String and pattern/pattern1.py
@@ -4,7 +4,72 @@
 # for i in range(3):
 #     print("*",end="")
 
-for i in range(3):
-    for j in range(3):
+# for i in range(3):
+#     for j in range(3):
+#         print("*",end="")
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="")
+#     print()
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(j,end="")
+#     print()
+
+
+# row=int(input("Enter row count="))
+# column=int(input("Enter column count="))
+# for i in range(row):
+#     for i in range(column):
+#         print("*",end="")
+#     print()
+
+
+#Another Example
+
+# row=int(input("Enter row count="))
+# column=int(input("Enter column count="))
+# for i in range(row):
+#     for j in range(column):
+#         if i==j:
+#             print("1",end="")
+#         else:
+#             print("0",end="")
+#     print()
+
+
+
+#Left-Incremental Triangle
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
         print("*",end="")
+    print()
+
+
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
+        print(i,end="")
+    print()
+
+
+
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
+        print(j,end="")
+    print()
+
+
+count=int(input("Enter row count="))
+n=1
+for i in range(count):
+    for j in range(i+1):
+        print(n,end="")
+        n+=1
     print()
